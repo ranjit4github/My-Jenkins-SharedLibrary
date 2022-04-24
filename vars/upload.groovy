091a7e0f6f8a0e313f76a904bfefeec65d8bc6d5
Ranjit Swain <58227542+ranjit4github@users.noreply.github.com>
2022-04-24 08:18:08 +0530

    Update upload.groovy

--- vars/upload.groovy
+++ vars/upload.groovy
@@ -1,6 +1,6 @@
 def call(){
   echo "Uploading the artifact..."
   sh '''
-    aws s3 cp ${workspace}/target/*.war s3://devopsweb.ansible
+    aws s3 cp ${WORKSPACE}/target/*.war s3://devopsweb.ansible
   '''
 }
