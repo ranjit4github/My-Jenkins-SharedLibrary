46dab0e946616ae9370a4947a7f3e1a568ab4c18
Ranjit Swain <58227542+ranjit4github@users.noreply.github.com>
2022-04-25 15:40:44 +0530

    Update deploy.groovy

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -1,6 +1,11 @@
 def call(){
-  echo "Executing deploy method....." 
-  sh '''
-    scp -v -o StrictHostKeyChecking=no **/*.war root@${staging_server}:/opt/tomcat/webapps/
-  '''
+  echo "Executing deploy method....."
+  
+  def server_list = "${staging_server}".replaceAll("\\s","")
+  
+  for (String server in server_list.split(',')){
+    sh '''
+      scp -v -o StrictHostKeyChecking=no **/*.war root@${server}:/opt/tomcat/webapps/
+    '''
+  }
 }
