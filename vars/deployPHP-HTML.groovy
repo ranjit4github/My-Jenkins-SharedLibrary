46230835ee3688f41b6253a9602b96b799884b07
Ranjit Swain <58227542+ranjit4github@users.noreply.github.com>
2022-10-15 12:41:23 +0530

    Update deployPHP-HTML.groovy

--- vars/deployPHP-HTML.groovy
+++ vars/deployPHP-HTML.groovy
@@ -4,7 +4,7 @@ def call(){
     for fileName in `find ${WORKSPACE} -type f -mmin -10 | egrep -v ".git|Jenkinsfile"`
     do
       fil=$(echo ${fileName} | sed 's/'"${JOB_NAME}"'/ /' | awk {'print $2'})
-      scp ${WORKSPACE}${fil} root@${staging_server}:/var/www/html/mbaquatech${fil}
+      scp ${WORKSPACE}${fil} root@${staging_server}:/var/www/html/${JOB_NAME}${fil}
     done
   '''
 }
