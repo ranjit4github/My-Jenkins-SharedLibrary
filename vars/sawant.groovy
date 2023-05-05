73a604ebeb5a07d5d3d653210315600e51fa6499
psawant27 <psawant27feb1990@gmail.com>
2023-05-05 17:19:54 +0530

    Update sawant.groovy

--- vars/sawant.groovy
+++ vars/sawant.groovy
@@ -1,4 +1,4 @@
 def call(){
-  echo "Executing build method...."
-  echo "I'm called out from Shared Library"
+  echo "Executing build method using shared libary to show case use of variables and done by prashant"
+  echo "This method used to show shared_library concept"
 }
