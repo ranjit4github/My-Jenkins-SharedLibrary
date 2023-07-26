79520ea019cfc3924bfda9a601bfb329bbe0da1b
Ranjit Swain <58227542+ranjit4github@users.noreply.github.com>
2023-07-26 14:41:47 +0530

    Update copy_PHP_HTML.groovy

--- vars/copy_PHP_HTML.groovy
+++ vars/copy_PHP_HTML.groovy
@@ -1,4 +1,4 @@
-def copyfile(){
+def copyfile1(){
   echo "Coping Application files"
   sh '''
     rsync -avzh ${WORKSPACE} --exclude 'Jenkinsfile' --exclude '.git' root@${staging_server}:/var/www/html/
