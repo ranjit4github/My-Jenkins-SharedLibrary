01902ae0de0fb9f5ad1e7ed11fe74c1564e0047b
Ranjit Swain <58227542+ranjit4github@users.noreply.github.com>
2023-09-21 18:28:12 +0530

    Update copy_PHP_HTML.groovy

--- vars/copy_PHP_HTML.groovy
+++ vars/copy_PHP_HTML.groovy
@@ -1,10 +1,11 @@
 def copyfile(){
   echo "Coping Application files"
-  if ("${env}" == "dev"){
+  echo "Envvironment: ${env}"
+  if (${env} == "dev"){
       sh '''
         rsync -avzh ${WORKSPACE} --exclude 'Jenkinsfile' --exclude '.git' root@${server}:/var/www/html/
       '''
-  }else if("${env}" == "prod")
+  }else if(${env} == "prod")
   {
       sh '''
         rsync -avzhe ${WORKSPACE} --exclude 'Jenkinsfile' --exclude '.git' root@${server}:/home/ec2-user/
