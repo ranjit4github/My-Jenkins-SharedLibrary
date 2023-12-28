e9e8b1b96a810ac5c1b167d6247bcf9961ba48e2
Ranjit Swain <58227542+ranjit4github@users.noreply.github.com>
2023-12-28 17:35:49 +0530

    Update gitCheckout.groovy

--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -3,15 +3,15 @@ def checkout (){
 }
 
 def push2Remote(){
-  bat '''
+  bat """
     cd "/d F:"
-    cd "F:\\mbgheri\\"
-    git config --global --add safe.directory F:/mbgheri
+    cd "F:\\${appName}\\"
+    git config --global --add safe.directory F:/${appName}
     git config --global user.email "${userEmail}"
     git config --global user.name "${userName}"
     git pull
     git add . 
     git commit -m "Commited by Jenkins"
-    git push origin master
-  '''
+    git push
+  """
 }
