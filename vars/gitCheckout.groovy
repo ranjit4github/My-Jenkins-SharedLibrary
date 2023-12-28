639215e83ba4d28a4d9b49c1a895ac65461ee4d2
Ranjit Swain <58227542+ranjit4github@users.noreply.github.com>
2023-12-28 16:15:11 +0530

    Update gitCheckout.groovy

--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -4,8 +4,8 @@ def checkout (){
 
 def push2Remote(){
   bat '''
-    cd /d F:
-    cd F:\\${appName}\\
+    cd "/d F:"
+    cd "F:\\${appName}\\"
     git config --global --add safe.directory F:/${appName}
     git config --global user.email "${userEmail}"
     git config --global user.name "${userName}"
