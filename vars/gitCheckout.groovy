133abe600630ae0f47c0e2aca1062d95ef3ed15a
Ranjit Swain <58227542+ranjit4github@users.noreply.github.com>
2023-12-28 15:45:10 +0530

    Update gitCheckout.groovy

--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -1,3 +1,15 @@
-def call (){
+def checkout (){
   git branch: "${branch}", credentialsId: 'patDemoToken1', url: "${repoURL}"
+}
+
+def push2Remote(){
+  bat '''
+    cd /d F:
+    cd F:\\mbgheri\\
+    git config --global user.email "${userEmail}"
+    git config --global user.name "${userName}"
+    git add . 
+    git commit -m "Updated as part of Build - ${BUILD_NUMBER}"
+    git push
+  '''
 }
